b4aebcd2fefe12dca6cdd65f4a40e6e8b5976857
GlebAlekseev <glebalekseevjk@gmail.com>
2020-11-12 15:00:33 +0300

    Version 1.01

---
+++ Practice/24/C++/example24/example24/example24.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "includes/json.hpp"
+#include <fstream>
+#include <vector>
+#include <string>
+
+
+using json = nlohmann::json;
+using namespace std;
+
+int main()
+{
+	json j;
+	ifstream src("in.json");
+
+	src >> j;
+
+	for (auto& x : j.items())
+	{
+		std::cout << "key: " << x.key() << ", value: " << x.value() << '\n';
+	}
+} 
+
+/// 
