d17fde7dbeba687d5a1c07415b573f55060329d5
GlebAlekseev <glebalekseevjk@gmail.com>
2020-12-22 16:44:51 +0300

    Version 1.01

---
+++ Practice/29/C++/example29/example29/example29.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	struct hi{
+		string name;
+		int group;
+	
+
+	};
+	const auto frt = hi{ "hh",3};
+	cout << frt.name;
+}
